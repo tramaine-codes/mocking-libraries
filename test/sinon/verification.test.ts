a68c087bdb417f7684b1d394f58a3671f9a00cd2
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2023-08-07 00:19:14 -0400

    refactor: reorganize codebase

---
+++ test/sinon/verification.test.ts
@@ -0,0 +1,22 @@
+import sinon from "sinon";
+import { afterEach, describe, test } from "vitest";
+import { Bar } from "../../src/bar";
+import { Foo } from "../../src/foo";
+
+const bar = sinon.createStubInstance(Bar);
+const log = sinon.fake(console.log);
+
+afterEach(() => {
+  sinon.reset();
+});
+
+describe("sinon", () => {
+  test("verify log is called", () => {
+    const foo = new Foo(bar, log);
+    bar.bar.returns("1");
+
+    foo.foo();
+
+    sinon.assert.calledWith(bar.baz, { bar: "1" }, log);
+  });
+});
---
+++ test/testdouble/verification.test.ts
@@ -0,0 +1,22 @@
+import * as td from "testdouble";
+import { afterEach, describe, test } from "vitest";
+import { Bar } from "../../src/bar";
+import { Foo } from "../../src/foo";
+
+const bar = td.instance(Bar);
+const log = td.func(console.log);
+
+afterEach(() => {
+  td.reset();
+});
+
+describe("testdouble", () => {
+  test("verify log is called", () => {
+    const foo = new Foo(bar, log);
+    td.when(bar.bar()).thenReturn("1");
+
+    foo.foo();
+
+    td.verify(bar.baz({ bar: "1" }, log));
+  });
+});
